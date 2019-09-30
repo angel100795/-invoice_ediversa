85be4bc26b8d79350bd38a3d4b64fbae6475000a
angel alvarez <angel.alvarez@quadit.mx>
2019-09-30 16:46:06 -0500

    Mensaje del commit

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'eDiversa Facturacion',
+    'version': '9.0.1.0.0',
+    'depends': [
+        'mail',
+        'sale',
+        'purchase'
+    ],
+    'author': 'Quadit, S.A. de C.V.',
+    'description': 'Factura eDiversa',
+    'website': 'https://www.quadit.mx',
+    'data': [
+        'views/factura_ediversa.xml',
+    ],
+    'demo': [],
+    'installable': True
+}
